370cac0b5835054c2e6b3245047c06666dfd98b2
Parrthii <parrthii@outlook.com>
2024-11-08 00:16:31 +0530

    2Update dynamo.tf

--- eks/dynamo.tf
+++ eks/dynamo.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_version = "~> 1.9.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   name = "terraform-state-lock-dynamo"
   hash_key = "LockID"
@@ -16,8 +8,4 @@ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
     name = "LockID"
     type = "S"
   }
-}
-
-provider "aws" {
-  region  = var.aws-region
 }
