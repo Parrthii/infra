3189ae16430c030df5c3f469c1f1f89ec23dc2a2
Parrthii <parrthii@outlook.com>
2024-11-08 11:05:08 +0530

    Update dynamo.tf

--- eks/dynamo.tf
+++ eks/dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
+  name = "Lock-Files"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
